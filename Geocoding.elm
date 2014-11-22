0233b6bb196a426c49d259e7f929926f4c30543f
Andrew Lee <smiley325squared@gmail.com>
2014-11-21 18:01:36 -0600

    Adding documentation.

--- Geocoding.elm
+++ Geocoding.elm
@@ -1,4 +1,21 @@
 module Geocoding where
+{-| Elm interface to the Google Maps Geocoding v3 API. Also includes interface to Static Maps API for map display. Names and metanames try as best as possible to conform to the original JavaScript API.
+
+The use of this library requires that the Google Maps JavaScript is embedded on your page with a working API key. This could be as easy as including a script tag in your document head (for elm-html users), or adding it as a compiler flag for pure Elm. Asynchronously mounting the JavaScript library is a little trickier -- inspect the example/ directory on the GitHub page for this library for details.
+
+# API Interface
+@docs byAddress, isFailure
+
+# Static Maps
+@docs mapUrl, calcZoom
+
+# Types and Constructors
+@docs Location, Bounds, Geometry, Address, Geocode, GeocodeRequest, GeocodeResponse, GeocoderStatus
+
+# Type Displays
+@docs showStatus, showLatitude, showLongitude
+
+-}
 
 import String
 import Dict
@@ -50,6 +67,10 @@ data GeocoderStatus
     | RequestDenied
     | InvalidRequest
 
+{-| Convert a GeocoderStatus into a human-readable string.
+
+    showStatus ZeroResults == "No results found."
+-}
 showStatus : GeocoderStatus -> String
 showStatus status =
     case status of 
@@ -60,6 +81,10 @@ showStatus status =
         InvalidRequest ->   "Invalid request."
         _ ->                "Unknown error."
 
+{-| Convert a location's latitude into a human-readable string.
+
+    showLatitude {lat: 60.5, lng: -30.3} == "N 60° 30′ 0″"
+-}
 showLatitude : Location -> String
 showLatitude location =
     let ns = if (location.lat > 0) then "N " else "S "
@@ -69,6 +94,10 @@ showLatitude location =
     in
         ns ++ show (floor d) ++ "° " ++ show (floor m) ++ "′ " ++ show (floor s) ++ "″"
 
+{-| Convert a location's longitude into a human-readable string.
+
+    showLongitude {lat: 60.5, lng: -30.3} == "W 30° 18′ 0″"
+-}
 showLongitude : Location -> String
 showLongitude location =
     let ns = if (location.lng > 0) then "E " else "W "
@@ -78,19 +107,30 @@ showLongitude location =
     in
         ns ++ show (floor d) ++ "° " ++ show (floor m) ++ "′ " ++ show (floor s) ++ "″"
 
+{-| Check if a GeocodeResponse was a failure or not. 
+    
+    isFailure Failure _ = True
+    isFailure Waiting = False
+    isFailure Success _ = False
+-}
 isFailure : GeocodeResponse -> Bool
 isFailure gr = 
     case gr of
         Failure _ -> True
         _ -> False
 
+{-| Interface to Google Maps Geocoder API. Given an address or location as a search term, returns a list of possible locations. As an asynchronous request, byAddress is structured as a Signal transformer, transforming GeocodeRequest signals to GeocodeResponses. It is recommended to debounce the input signal, otherwise you may get receive OverQueryLimit from Google (rate limit).
+
+    byAddress {address="Chicago, IL"}
+-}
 byAddress : Signal GeocodeRequest -> Signal GeocodeResponse
 byAddress = Native.Geocoding.byAddress
 
 -- Display Maps
 -- https://developers.google.com/maps/documentation/staticmaps/
 
--- Turn lat/long coords + zoom into embedded Google Map link
+{-| Get the URL for a static Google map, given a location, zoom, and image dimensions. The URL can then be set on an Image element for display. Zoom ranges from 1-12, with 1 being the most zoomed out.
+-}
 mapUrl : Location -> Int -> (Int, Int) -> String
 mapUrl coords zoom (width, height) = 
     "https://maps.googleapis.com/maps/api/staticmap?" 
@@ -98,7 +138,11 @@ mapUrl coords zoom (width, height) =
         ++ "zoom=" ++ (show zoom) ++ "&"
         ++ "size=" ++ (show width) ++ "x" ++ (show height)
 
--- Calculate zoom from map bounds and desired pixel width
+{-| Calculate the zoom factor from viewport bounds. This is helpful when deciding what zoom to pass to mapUrl, when you have a Geocode from byAddress. The canonical usage is as follows. Notice how the width is repeated twice.
+
+    gc = Geocode ...
+    mapUrl gc.geometry.location (calcZoom gc.geometry.viewport 500) (500, 300)
+-}
 calcZoom : Bounds -> Int -> Int
 calcZoom viewport displayWidth =
     let globeWidth = 256.0  -- a constant in Google Maps' projection
--- README.md
+++ README.md
@@ -6,10 +6,10 @@ Requires Google Maps API in global scope.
 
 ![](https://raw.githubusercontent.com/smiley325/elm-geocoding/master/Demo.gif)
 
-Build and Run Test
-------------------
+Build and Run Example
+---------------------
 
-To build the test harness, run `elm-get install` followed by `make.sh`. Change into the `test` directory and run `python -m http.server` (Python 3) or `python -m SimpleHTTPServer` (Python 2). Navigate to `localhost:8000/test.html` in your browser of choice.
+To build the example, run `elm-get install` followed by `make.sh`. Change into the `example` directory and run `python -m http.server` (Python 3) or `python -m SimpleHTTPServer` (Python 2). Navigate to `localhost:8000/example.html` in your browser of choice.
 
 The test will require that you input a Google API key, which can be acquired from the Google Developer Center. Make sure to set your allowed referers, otherwise Google Maps API will complain.
 
